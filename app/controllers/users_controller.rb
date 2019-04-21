e7b4c4c94fcab2947e6edb5b5d942263394618dd
Kevin Lee <pvklee@ucla.edu>
2019-04-21 16:00:40 -0700

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    before_action :require_current_user!, except: [:create, :new]
+    
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages
+        end
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
